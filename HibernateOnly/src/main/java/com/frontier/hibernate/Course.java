651fceca64873d20cacd2e348f59d31b2b08ce39
repaka <aravindin4u@gmail.com>
2014-01-27 23:29:44 -0600

    Added Comments

--- HibernateOnly/src/main/java/com/frontier/hibernate/Course.java
+++ HibernateOnly/src/main/java/com/frontier/hibernate/Course.java
@@ -5,7 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+/**
+ * 
+ * @author arepaka
+ *
+ */
 @Entity
 @Table(name="COURSE")
 public class Course {
